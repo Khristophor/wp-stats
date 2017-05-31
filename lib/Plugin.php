bba7f86ee4d0c61025b0df1e7059964d1e79fcf6
Chris Wiseman <wiseman.christopher@gmail.com>
2017-05-31 20:56:12 -0300

    Add Loader hook to register the StatWidget in the WordPress admin area

--- lib/Plugin.php
+++ lib/Plugin.php
@@ -101,6 +101,7 @@ class Plugin {
 
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_styles' );
 		$this->loader->add_action( 'admin_enqueue_scripts', $plugin_admin, 'enqueue_scripts' );
+		$this->loader->add_action( 'widgets_init', $plugin_admin, 'register_widgets' );
 
 	}
 
