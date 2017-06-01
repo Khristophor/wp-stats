a007f3e2b356593b803a9d12e33e2d8aa2979e10
Chris Wiseman <wiseman.christopher@gmail.com>
2017-05-31 22:40:07 -0300

    Modify markup in StatWidget::widget method to display custom statistics

--- lib/Admin/StatWidget.php
+++ lib/Admin/StatWidget.php
@@ -66,6 +66,22 @@ class StatWidget extends \WP_Widget {
 	 */
 	public function widget( $args, $instance ) {
 		$title = \apply_filters( 'widget_title', $instance['title'] );
+		echo $args['before_widget'] . $args['before_title'] . esc_attr( $title ) . $args['after_title'];
+		$stats = $this->collect_stats(); ?>
+		<h5>Counts</h5>
+		<p><strong>Posts:</strong> <?php echo esc_attr( $stats['post_count'] ); ?></p>
+		<p><strong>Pages:</strong> <?php echo esc_attr( $stats['page_count'] ); ?></p>
+		<p><strong>Authors:</strong> <?php echo esc_attr( $stats['author_count'] ); ?></p>
+		<h5>Most Popular</h5>
+		<p><strong>Category:</strong> <?php echo esc_attr( current( array_keys( $stats['category'] ) ) ); ?></p>
+		<p class="aside"><?php echo esc_attr( current( array_values( $stats['category'] ) ) ); ?> posts tagged.</p>
+		<p><strong>Tag:</strong> <?php echo esc_attr( current( array_keys( $stats['tag'] ) ) ); ?></p>
+		<p class="aside"><?php echo esc_attr( current( array_values( $stats['tag'] ) ) ); ?> posts tagged.</p>
+		<p><strong>Post:</strong> <a href="<?php echo get_permalink( $stats['commented']['post_id'] ); ?>"><?php echo esc_attr( $stats['commented']['post_title'] ); ?></a></p>
+		<p class="aside"><?php echo esc_attr( $stats['commented']['comment_count'] ); ?> comments.</p>
+		<?php echo $args['after_widget'];
+	}
+
 	/**
 	 * Call custom REST endpoints registered to Common\StatController and collect
 	 * statistics.
