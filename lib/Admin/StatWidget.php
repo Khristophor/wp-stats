3b70cefb9bbea978b51fdb4d678323e22af263a7
Chris Wiseman <wiseman.christopher@gmail.com>
2017-06-05 23:45:38 -0300

    Modify the widget output and REST API call to use the updated single endpoint and Stat class

--- lib/Admin/StatWidget.php
+++ lib/Admin/StatWidget.php
@@ -69,43 +69,44 @@ class StatWidget extends \WP_Widget {
 		echo $args['before_widget'] . $args['before_title'] . esc_attr( $title ) . $args['after_title'];
 		$stats = $this->collect_stats(); ?>
 		<h5>Counts</h5>
-		<p><strong>Posts:</strong> <?php echo esc_attr( $stats['post_count'] ); ?></p>
-		<p><strong>Pages:</strong> <?php echo esc_attr( $stats['page_count'] ); ?></p>
-		<p><strong>Authors:</strong> <?php echo esc_attr( $stats['author_count'] ); ?></p>
+		<?php
+		foreach ( $stats as $stat ) {
+			if ( 'count' === $stat['type'] ) {
+				?>
+				<p><strong><?php echo esc_attr( $stat['label'] ); ?>:</strong> <?php echo esc_attr( $stat['value'] ); ?></p>
+				<?php
+			}
+		}
+		?>
 		<h5>Most Popular</h5>
-		<p><strong>Category:</strong> <?php echo esc_attr( current( array_keys( $stats['category'] ) ) ); ?></p>
-		<p class="aside"><?php echo esc_attr( current( array_values( $stats['category'] ) ) ); ?> posts tagged.</p>
-		<p><strong>Tag:</strong> <?php echo esc_attr( current( array_keys( $stats['tag'] ) ) ); ?></p>
-		<p class="aside"><?php echo esc_attr( current( array_values( $stats['tag'] ) ) ); ?> posts tagged.</p>
-		<p><strong>Post:</strong> <a href="<?php echo get_permalink( $stats['commented']['post_id'] ); ?>"><?php echo esc_attr( $stats['commented']['post_title'] ); ?></a></p>
-		<p class="aside"><?php echo esc_attr( $stats['commented']['comment_count'] ); ?> comments.</p>
+		<?php
+		foreach ( $stats as $stat ) {
+			if ( 'popular' === $stat['type'] ) {
+				?>
+				<p><strong><?php echo esc_attr( $stat['label'] ); ?>:</strong> <a href="<?php echo esc_attr( $stat['url'] ); ?>"><?php echo esc_attr( $stat['value'] ); ?></a></p>
+				<p class="aside"><?php echo esc_attr( $stat['tagline'] ); ?></p>
+				<?php
+			}
+		}
+		?>
 		<?php echo $args['after_widget'];
 	}
 
 	/**
-	 * Call custom REST endpoints registered to Common\StatController and collect
+	 * Call custom REST endpoint registered to Common\StatController and collect
 	 * statistics.
 	 *
 	 * @since 1.0.0
+	 * @return array $stats multidimensional array of statistics
 	 */
 	protected function collect_stats() {
 
-		$counts_endpoint = \get_home_url() . '/wp-json/cw/v1/counts';
-		$leaders_endpoint = \get_home_url() . '/wp-json/cw/v1/leaders';
+		$stats_endpoint = \get_home_url() . '/wp-json/cw/v1/stats';
 
-		$stats = array();
-
-		$response = wp_remote_get( $counts_endpoint );
+		$response = \wp_remote_get( $stats_endpoint );
 		if ( is_array( $response ) ) {
-			$stats = array_merge( $stats, json_decode( $response['body'], true ) );
+			return json_decode( $response['body'], true );
 		}
-
-		$response = wp_remote_get( $leaders_endpoint );
-		if ( is_array( $response ) ) {
-			$stats = array_merge( $stats, json_decode( $response['body'], true ) );
-		}
-
-		return $stats;
 
 	}
 
